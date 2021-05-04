0b209e498c55af5cef05595d4365b2d1ac830dec
Revadike <revadike@outlook.com>
2021-05-04 18:58:30 +0200

    Create AKS - List to JSON.js

---
+++ AKS - List to JSON.js
@@ -0,0 +1,10 @@
+console.log(JSON.stringify([...document.querySelectorAll("tr.game-row")].map(e => ({
+gameId: e.dataset.gameId,
+productId: e.querySelector(".metacritic-button").dataset.productId,
+name: e.querySelector(".game-name").innerText.trim(),
+metacriticScore: Number(e.querySelector(".metacritic-button").innerText.trim()),
+steamPriceEUR: Number((e.querySelector(".game-steam-price").innerHTML.split("<br>")[1]||"").trim().replace("€", "")),
+aksPriceEUR: Number(e.querySelector(".game-lowest-price a").innerText.trim().replace("€", "")),
+url: e.querySelector("td:nth-child(2) > a").href,
+img: e.querySelector("td:nth-child(2) > a > img").src
+}))))
