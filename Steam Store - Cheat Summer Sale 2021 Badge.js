3271f5d28ad51b31de17597f6e356e5969748563
Revadike <revadike@outlook.com>
2021-06-25 00:01:25 +0200

    Update Steam Store - Cheat Summer Sale 2021 Badge.js

--- Steam Store - Cheat Summer Sale 2021 Badge.js
+++ Steam Store - Cheat Summer Sale 2021 Badge.js
@@ -1,14 +1,15 @@
 /*
  * Made by Revadike (License MIT)
  *
- * The Masked Avenger (badge 51) = choices sum 14-16
- * The Trailblazing Explorer (badge 52) = choices sum 17-19
- * The Gorilla Scientist (badge 53) = choices sum 20-22
- * The Paranormal Professor (badge 54) = choices sum 23-25
- * The Ghost Detective (badge 55) = choices sum 26-28
+ * The Masked Avenger (badge 51) = sum choices must be 14-16
+ * The Trailblazing Explorer (badge 52) = sum choices must be 17-19
+ * The Gorilla Scientist (badge 53) = sum choices must be 20-22
+ * The Paranormal Professor (badge 54) = sum choices must be 23-25
+ * The Ghost Detective (badge 55) = sum choices must be 26-28
 */
 
-let choices = [1,1,1,1,1,1,1,1,1,1,1,1,1,1]; // make sure the sum matches your desired badge!
+// Your 14 preferred story choices:
+let choices = [2,2,2,2,2,2,2,2,2,2,2,2,2,2];
 
 for (let i = 1; i <= 14; i++) {
     jQuery.post("https://store.steampowered.com/promotion/ajaxclaimstickerforgenre", { genre: i, choice: choices[i-1], sessionid: g_sessionID }, (res) => console.log("genre " + i + ":", res))
