993913eaf222787b2c9d1102d6752955cce74cbe
Revadike <revadike@outlook.com>
2022-06-23 22:41:57 +0200

    Create Steam - Summar Sale 2022.js

---
+++ Steam - Summar Sale 2022.js
@@ -0,0 +1,40 @@
+/* eslint-env browser */
+/* global g_sessionID jQuery */
+
+(async() => {
+    let delay = (ms) => new Promise((res) => setTimeout(res, ms));
+    await jQuery.post("/saleaction/ajaxopendoor", {
+        "sessionid":      g_sessionID,
+        "authwgtoken":    jQuery("#application_config").data("userinfo").authwgtoken,
+        "door_index":     0,
+        "clan_accountid": 41316928,
+    });
+    for (let link of [
+        "/category/arcade_rhythm/?snr=1_614_615_clorthaxquest_1601",
+        "/category/strategy_cities_settlements/?snr=1_614_615_clorthaxquest_1601",
+        "/category/sports/?snr=1_614_615_clorthaxquest_1601",
+        "/category/simulation/?snr=1_614_615_clorthaxquest_1601",
+        "/category/multiplayer_coop/?snr=1_614_615_clorthaxquest_1601",
+        "/category/casual/?snr=1_614_615_clorthaxquest_1601",
+        "/category/rpg/?snr=1_614_615_clorthaxquest_1601",
+        "/category/horror/?snr=1_614_615_clorthaxquest_1601",
+        "/vr/?snr=1_614_615_clorthaxquest_1601",
+        "/category/strategy/?snr=1_614_615_clorthaxquest_1601",
+    ]) {
+        try {
+            let html = await jQuery.get(link);
+            await jQuery.post("/saleaction/ajaxopendoor", {
+                "sessionid":      g_sessionID,
+                "authwgtoken":    jQuery("#application_config", html).data("userinfo").authwgtoken,
+                "door_index":     jQuery("#application_config", html).data("capsuleinsert").payload,
+                "clan_accountid": 41316928,
+                "datarecord":     jQuery("#application_config", html).data("capsuleinsert").datarecord,
+            });
+            console.log("You got a new badge!");
+        } catch (e) {
+            console.error("Failed to obtain badge!", e);
+        } finally {
+            await delay(1500);
+        }
+    }
+})();
