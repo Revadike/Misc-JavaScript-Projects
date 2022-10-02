47a3791d35c272ac9d6dde5599bc6466f9966e9e
Revadike <revadike@outlook.com>
2022-10-03 00:03:47 +0200

    Update Origin - Freebies Claimer.js

--- Origin - Freebies Claimer.js
+++ Origin - Freebies Claimer.js
@@ -1,5 +1,8 @@
 // https://www.origin.com/game-library
 
+const COUNTRY_OVERRIDE = null; // e.g. "NL"
+const LOCALE_OVERRIDE = null; // e.g. "en_NL"
+
 (async () => {
     let authtoken = Origin.user.accessToken();
     let { pid } = await fetch("https://gateway.ea.com/proxy/identity/pids/me", {
@@ -22,7 +25,12 @@
     }).then(res => res.json());
     let { pidId, locale, country } = pid;
     country = locale.includes(country) ? country : locale.split("_")[1];
-    let { offers } = await fetch(`https://api1.origin.com/supercat/${country}/${locale}/supercat-PCWIN_MAC-${country}-${locale}.json.gz`, { credentials: "omit" }).then(res => res.json());
+    country = COUNTRY_OVERRIDE || country;
+    locale = LOCALE_OVERRIDE || locale;
+    let { offers } = await fetch(`https://api1.origin.com/supercat/${country}/${locale}/supercat-PCWIN_MAC-${country}-${locale}.json.gz`, { credentials: "omit" }).then(res => res.json()).catch(() => ({}));
+    if (!offers) {
+        ({ offers } = await fetch("https://api1.origin.com/supercat/US/en_US/supercat-PCWIN_MAC-US-en_US.json.gz", { credentials: "omit" }).then(res => res.json()));
+    }
     let offersToRedeem = offers.filter(e => e.isZeroPricedOffer === "true").map(e => e.offerId);
 //     let offersToRedeem = [...new Set(offers.map(e => e.offerId).concat(offers.map(e => e.extraContent || []).flat()))]
     for (let id of offersToRedeem) {
